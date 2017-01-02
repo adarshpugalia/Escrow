b98ce683a3567ba79826d1bbd95e0d69595c4b54
Adarsh Pugalia <adarsh@localhost.localdomain>
2017-01-03 01:44:50 +0530

    Added navigation drawer to the main activity.

--- app/src/main/java/com/taadnairsshha/escrow/MainActivity.java
+++ app/src/main/java/com/taadnairsshha/escrow/MainActivity.java
@@ -1,17 +1,23 @@
 package com.taadnairsshha.escrow;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.taadnairsshha.escrow.Utils.CommonUtils;
 import com.taadnairsshha.escrow.activities.AddAcountActivity;
 import com.taadnairsshha.escrow.activities.AddCouponActivity;
 import com.taadnairsshha.escrow.activities.AddWebsiteActivity;
-import com.taadnairsshha.escrow.activities.CouponsActivity;
 import com.taadnairsshha.escrow.data.EscrowDbHelper;
 import com.taadnairsshha.escrow.data.service.DbWebsiteService;
 import com.taadnairsshha.escrow.models.Website;
@@ -19,38 +25,115 @@ import com.taadnairsshha.escrow.models.Website;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private String navOptionStrings[] = {"Coupons", "Accounts", "Websites"};
+    private DrawerLayout drawerLayout;
+    private ListView navDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView) findViewById(R.id.activity_main_textview_test);
+        /* Adding the navigation drawer to the activity. */
+        addNavigationDrawer();
 
-        EscrowDbHelper escrowDbHelper = new EscrowDbHelper(getApplicationContext());
-        SQLiteDatabase db = escrowDbHelper.getWritableDatabase();
+        /* setting the listeners on the navigation drawer. */
+        setNavigationDrawerListeners();
 
-        List<Website> websiteList = DbWebsiteService.getWebsites(db);
-        for(Website website: websiteList) {
-            textView.append(website.getName() + "\n\n");
+        /* set the action bar. */
+        setActionBar();
+    }
+
+
+    /* This method add the navigation bar to the activity. */
+    private void addNavigationDrawer() {
+        /* getting the views from layout. */
+        drawerLayout = (DrawerLayout) findViewById(R.id.act_main_drawer_layout);
+        navDrawerList = (ListView) findViewById(R.id.act_main_nav_drawer_layout);
+
+        /* Setting the adapter for the list view. */
+        navDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navOptionStrings));
+
+        /* setting the on click listener. */
+        navDrawerList.setOnItemClickListener(new NavigationDrawerClickListener());
+    }
+
+
+    /* This method sets up the navigation drawer listener. */
+    private void setNavigationDrawerListeners() {
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle("Escrow");
+            }
+
+            public void onDrawerOpened(View view) {
+                super.onDrawerOpened(view);
+                getSupportActionBar().setTitle("Nav Drawer");
+            }
+
+        };
+
+        /* setting the drawer toggle as the drawer listener. */
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+    }
+
+
+    /* This method sets the action bar on the activity. */
+    public void setActionBar() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
-
-        textView.append(String.valueOf(websiteList.size()));
+        // Handle your other action bar items...
+        return super.onOptionsItemSelected(item);
     }
 
-    public void goToAddWebsite(View view) {
-        CommonUtils.startActivity(this, AddWebsiteActivity.class);
+
+    /* This class implements the onclick listener for the navigation bar items. */
+    private class NavigationDrawerClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectNavigationDrawerItem(position);
+        }
     }
 
-    public void goToAddAccount(View view) {
-        CommonUtils.startActivity(this, AddAcountActivity.class);
-    }
 
-    public void goToAddCoupon(View view) {
-        CommonUtils.startActivity(this, AddCouponActivity.class);
-    }
+    /* This method is called when an item in the navigation drawer is clicked. */
+    private void selectNavigationDrawerItem(int position) {
+        /* setting the selected item to checked. */
+        navDrawerList.setItemChecked(position, true);
 
-    public void goToCoupons(View view) {
-        CommonUtils.startActivity(this, CouponsActivity.class);
+        /* setting the title for the activity. */
+        setTitle(navOptionStrings[position]);
+
+        /* closing the navigation drawer. */
+        drawerLayout.closeDrawer(navDrawerList);
     }
 }
